0fc12ee17bcf6c1a1785a0c7c63bf6930f57d32b
dswenningsen <dswenningsen@gmail.com>
2025-11-11 12:01:39 -0800

    yup

--- src/scheduled_rule_template.py
+++ src/scheduled_rule_template.py
@@ -10,10 +10,16 @@ Reference:
 
 # pylint: disable=C0103, R0903, E0401, E0213
 
-from typing import List
+from typing import List, Dict
 from enum import Enum
-from pydantic import BaseModel, Field, model_validator
-from pydantic.json_schema import SkipJsonSchema
+from pydantic import (
+    BaseModel,
+    Field,
+    model_validator,
+    field_serializer,
+    field_validator,
+    ValidationError,
+)
 import scheduled_rule as sr
 
 
@@ -32,35 +38,85 @@ class TemplateStatus(str, Enum):
     NotAvailable = "NotAvailable"
 
 
-# TODO: Fix inheritance
-class ScheduledAlertRuleTemplateProperties(sr.ScheduledAlertRuleProperties):
+class ScheduledAlertRuleTemplateProperties(BaseModel):
     """Model"""
 
-    enabled: SkipJsonSchema[bool] = Field(default=False)
-    suppressionEnabled: SkipJsonSchema[bool] = Field(default=False)
-    suppressionDuration: SkipJsonSchema[str] = Field(default="PT5M")
+    alertDetailsOverride: sr.AlertDetailsOverride | None = None
+    customDetails: Dict[str, str] | None = None
+    description: str | None = None
+    displayName: str | None = None
+    entityMappings: List[sr.EntityMapping] | None = None
+    eventGroupingSettings: sr.EventGroupingSettings | None = None
+    query: str
+    queryFrequency: str
+    queryPeriod: str
     requiredDataConnectors: List[AlertRuleTemplateDataSources] | None = None
+    severity: sr.AlertSeverity
     status: TemplateStatus | None = None
+    subTechniques: List[str] | None = None
+    tactics: List[sr.AttackTactic] | None = None
+    techniques: List[str] | None = None
+    triggerOperator: sr.TriggerOperator | None = None
+    triggerThreshold: int | None = None
+    version: str
 
-    # TODO: Does this need to be a SkipJsonSchema?
-    version: SkipJsonSchema[str | None] = Field(default=None, exclude=True)
+    class Config:
+        """Config"""
+
+        use_enum_values = True
+
+    @field_serializer("queryFrequency", "queryPeriod", "suppressionDuration")
+    def serialize_duration(self, v: str) -> str:
+        """validate"""
+        return sr.to_iso8601_duration(v)
 
     @model_validator(mode="before")
-    def alter_template_version(cls, values):
-        """Alter templateVersion to version."""
-        # TODO: Fix problem Here
-        try:
-            if values["version"] is not None and (
-                "templateVersion" not in values
-                or values["templateVersion"] is None
-            ):
-                values["templateVersion"] = values["version"]
-            return values
-        except KeyError:
-            return values
+    def validate_techniques(self) -> list[str]:
+        """validate"""
+        validated_techniques = []
+        sub_techniques = []
+        if "techniques" in self and self["techniques"] is not None:
+            for tech in self["techniques"]:
+                if "." in tech:
+                    sub_techniques.append(tech)
+                    validated_techniques.append(tech.split(".")[0])
+                else:
+                    validated_techniques.append(tech)
+            self["techniques"] = list(set(validated_techniques))
+            if "subTechniques" in self and self["subTechniques"] is None:
+                self["subTechniques"] = list(set(sub_techniques))
+            if "subtechniques" in self and self["subtechniques"] is None:
+                self["subTechniques"] = list(set(sub_techniques))
+                self.pop("subtechniques")
+        return self
+
+    @field_validator("triggerOperator", mode="before")
+    @classmethod
+    def validate_trigger_operator(cls, value: str) -> str:
+        """validate"""
+        mapping = {
+            "eq": "Equal",
+            "ne": "NotEqual",
+            "gt": "GreaterThan",
+            "lt": "LessThan",
+            "equals": "Equal",
+            "notequals": "NotEqual",
+            "greaterthan": "GreaterThan",
+            "lessthan": "LessThan",
+        }
+        if value in mapping:
+            return mapping[value]
+        elif value in mapping.values():
+            return value
+        raise ValidationError(f"Invalid value for triggerOperator: {value}")
 
 
-class ScheduledAlertRuleTemplate(sr.ScheduledAlertRule):
+class ScheduledAlertRuleTemplate(BaseModel):
     """Model"""
 
+    id: str | None = None
+    name: str | None = None
+    type: str | None = None
+    systemData: sr.SystemData | None = None
+    kind: str = Field(default="Scheduled")
     properties: ScheduledAlertRuleTemplateProperties
