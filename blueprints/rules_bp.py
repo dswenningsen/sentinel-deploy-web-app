acea287d26c2947f290d0ccc33f5c7896807af70
dswenningsen <dswenningsen@gmail.com>
2025-11-12 22:02:44 -0800

    removed unneeded code

--- blueprints/rules_bp.py
+++ blueprints/rules_bp.py
@@ -21,41 +21,6 @@ deploy_rules_bp = Blueprint("deploy_rules", __name__)
 rule_deployments = {}
 
 
-@deploy_rules_bp.route("/deploy_rules", methods=["GET", "POST"])
-def deploy_rules():
-    """Display a page to trigger rule deployment and start background task."""
-    logger.info("Rendering deploy_rules page.")
-
-    if request.method == "POST":
-        logger.info("Deploy rules form submitted.")
-        workspace_form = session.get("workspace_form")
-        client_secret = session.get("client_secret")
-        deployment_id = str(uuid.uuid4())
-        rule_deployments[deployment_id] = {"status": "In Progress", "logs": []}
-
-        logger.info(
-            f"Starting rule deployment thread for deployment_id={deployment_id}"
-        )
-        thread = threading.Thread(
-            target=deploy_rules_task,
-            args=(
-                deployment_id,
-                workspace_form,
-                client_secret,
-                rule_deployments,
-            ),
-        )
-        thread.start()
-        logger.info(
-            f"Redirecting to monitor_rules for deployment_id={deployment_id}"
-        )
-        return redirect(
-            url_for("deploy_rules.monitor_rules", deployment_id=deployment_id)
-        )
-
-    return render_template("deploy_rules.html")
-
-
 @deploy_rules_bp.route(
     "/deploy_rules_prompt/<source_deployment_id>", methods=["GET", "POST"]
 )
